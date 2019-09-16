06cdabd5a06765e75ae088b66fcad975d6dbfb78
Adam Brown <downtownadambrown@gmail.com>
2019-09-16 15:03:21 -0400

    new build

---
+++ precache-manifest.0461c1322a0e322e32ef9a04273189f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a256e8785acb3a46b7c29d672534af4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e26863a8dca3e47dbb55",
+    "url": "./static/css/main.ec053430.chunk.css"
+  },
+  {
+    "revision": "448b6d89210a5103f846",
+    "url": "./static/js/2.d4d554de.chunk.js"
+  },
+  {
+    "revision": "e26863a8dca3e47dbb55",
+    "url": "./static/js/main.7c42ec6a.chunk.js"
+  },
+  {
+    "revision": "bd065af9a743fb1dc82c",
+    "url": "./static/js/runtime~main.02b66575.js"
+  }
+]);
